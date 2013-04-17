59570872e0a65f98d0bc067547b7257cb9bbc2d8
jessicajunk <jajunk@uw.edu>
2013-04-16 20:10:17 -0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,25 @@
-def find_fib(n):
-    return n
+"""This function takes in an int n and returns the nth number in the
+Fibonacci Sequence, where the sequence starts with 0, 1, 1, 2, ..."""
 
-print find_fib(3)
+def find_fib(n):
+    if n > 0:
+        if n < 3:
+            return n - 1
+        else:
+            count = 2
+            last = 0
+            sum = 1
+            while count < n:
+                count += 1
+                hold = sum
+                sum += last
+                last = hold
+            return sum
+    else:
+        raise Exception("n must be a positive integer.")
+
+if __name__ == '__main__':
+    x = int(raw_input("Input a positive integer: "))
+    print "Here are the first %d numbers in the Fibonacci Sequence:" % x
+    for i in range(x):
+        print find_fib(i + 1)
